c7628ced744ca88ba2e02e3360b6fcd3033201fc
harryrussin <harryrussin2@gmail.com>
2022-02-04 19:53:03 +0000

    checking for valid words2

--- words.js
+++ words.js
@@ -1,8 +1,9 @@
 module.exports = {
-    goalwords: ['grill', 'house', 'mouse', 'clear', 'clean', 'photo', 'farms', 'alarm', 'karma', 'sword', 'words', 'harry', 'roads', 'oiled', 'theft', 'crumb', 'drone', 'snowy', 'likes', 'krill', 'fairy'
-        , 'green', 'cabin', 'cable', 'deals', 'daddy', 'mommy', 'mummy', 'fable', 'table', 'sweet', 'labor', 'label', 'macho'
-        , 'moist', 'oasis', 'rabid', 'vague'],
+    goalwords: ['grill', 'house', 'mouse', 'clear', 'clean', 'photo', 'farms', 'alarm', 'karma', 'sword', 'words', 'harry', 'roads', 'oiled', 'coral', 'theft', 'crumb', 'drone', 'snowy', 'likes', 'krill', 'fairy'
+        , 'green', 'cabin', 'cable', 'choux', 'crazy', 'deals', 'fable', 'table', 'abbey', 'sweet', 'labor', 'label', 'macho'
+        , 'moist', 'oasis', 'rabid', 'vague', 'pazaz', 'jazzy', 'whizz', 'dizzy', 'pizza', 'other', 'about', 'which'],
     checkwords: ['grill', 'house', 'mouse', 'clear', 'clean', 'photo', 'farms', 'alarm', 'karma', 'sword', 'words', 'harry', 'roads', 'oiled', 'theft', 'crumb', 'drone', 'snowy', 'likes', 'krill', 'fairy'
         , 'green', 'cabin', 'cable', 'deals', 'daddy', 'mommy', 'mummy', 'fable', 'table', 'sweet', 'labor', 'label', 'macho'
-        , 'moist', 'oasis', 'rabid', 'vague','hills']
+        , 'moist', 'oasis', 'rabid', 'vague', 'hills', 'pazaz', 'jazzy', 'whizz', 'dizzy', 'pizza', 'mezze', 'mezzo', 'other', 'about', 'which', 'their', 'every', 'faith', 'lunch', 'maybe', 'clown', 'cloth', 'owlet', 'scowl', 'towel', 'seize', 'serve', 'sharp', 'shelf', 'shine', 'solid', 'eager'
+        , 'earth', 'ebony', 'eight', 'elect', 'choux', 'crazy', 'elite', 'entry', 'extra', 'abate', 'abbey', 'abort', 'abrim', 'chonk', 'champ', 'click', 'clock', 'clump', 'chomp', 'cliff', 'climb', 'chalk', 'coral']
 }
